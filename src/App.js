7a611d5c7bfa6f73291ffea0c8a43bb90b837a3f
Haavard Haug <haavardhaug@gmail.com>
2021-11-10 22:30:28 +0100

    Add analog clock

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import logo from './gear.png';
 import './App.css';
 import Clock from './Clock';
+import AnalogClock from 'analog-clock-react';
+
+let options = {
+  width: "300px",
+  border: true,
+  borderColor: "#2e2e2e",
+  baseColor: "#17a2b8",
+  centerColor: "#459cff",
+  centerBorderColor: "#ffffff",
+  handColors: {
+    second: "#d81c7a",
+    minute: "#ffffff",
+    hour: "#ffffff"
+  }
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <h2>
           app.motoren.site
         </h2>
+        <AnalogClock {...options} />
         <Clock/>
       </header>
     </div>
